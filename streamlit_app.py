985cb578933c43957d38debfb1bd8ba5ee34a915
Jianghui-Li <54603529+Jianghui-Li@users.noreply.github.com>
2024-09-05 14:58:53 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 
-st.set_page_config(page_title="Lab Navigation App", layout="centered")
+lab1 = st.Page("lab1.py", title= "lab1")
+lab2 = st.Page("lab2.py", title= "lab2")
 
-page = st.selectbox("Choose a lab page", ("Lab 1", "Lab 2"))
-
-if page == "Lab 1":
-    st.write("# Lab 1: Document Upload and Question Answering")
-    import pages.lab1
-elif page == "Lab 2":
-    st.write("# Lab 2: Another Type of Analysis")
-    import pages.lab2
+pg = st.navigation([lab1, lab2])
+st.set_page_config(page_title="Document", page_icon=":material/edit:")
+pg.run()
