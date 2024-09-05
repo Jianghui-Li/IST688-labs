9840e08e36fb7dbb82a8c7d5ef4b4c9062b8caef
Jianghui-Li <54603529+Jianghui-Li@users.noreply.github.com>
2024-09-05 14:29:24 +0000

    update

--- pages/lab2.py
+++ pages/lab2.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Another Analysis Page")
+st.title("Another Page")
 
-st.write("This is another page where you can perform a different type of document analysis or ask another set of questions.")
+st.write("Whatever man.")
