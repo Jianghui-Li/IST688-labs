b1b19cfac54c903fba2e788320edd48ed7514734
Jianghui-Li <54603529+Jianghui-Li@users.noreply.github.com>
2024-09-05 14:46:22 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,3 @@ st.set_page_config(page_title="Lab Navigation", layout="centered")
 st.sidebar.title("Lab Navigation")
 selected_lab = st.sidebar.selectbox("Choose a Lab", ["Lab 1", "Lab 2"])
 
-# Display the selected page based on navigation
-if selected_lab == "Lab 1":
-    st.write("You are on **Lab 1** page")
-    # Importing Lab 1 as a function or code block
-    import lab1
-    lab1.display_lab1()
-elif selected_lab == "Lab 2":
-    st.write("You are on **Lab 2** page")
-    # Importing Lab 2 as a function or code block
-    import lab2
-    lab2.display_lab2()
