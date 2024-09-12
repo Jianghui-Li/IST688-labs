8bdeb02c34eed8f036b3673059530ac3ac1f05b7
Jianghui-Li <54603529+Jianghui-Li@users.noreply.github.com>
2024-09-12 14:24:41 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 lab1 = st.Page("lab1.py", title= "lab1")
 lab2 = st.Page("lab2.py", title= "lab2")
+lab3 = st.Page("lab3.py", title= "lab3")
 
-pg = st.navigation([lab1, lab2])
+pg = st.navigation([lab1, lab2, lab3])
 st.set_page_config(page_title="Document", page_icon=":material/edit:")
 pg.run()
